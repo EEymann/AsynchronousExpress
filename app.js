3b49b17132d045c38118049a0068b67d4003864b
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-08 23:48:50 -0700

    Refactor using async/await.

--- app.js
+++ app.js
@@ -41,14 +41,23 @@ function getUsers(){
   });
 }
 
-app.get('/', (req,res) => {
-  getUsers()
-  .then((users)=> {
+// app.get('/', (req,res) => {
+//   getUsers()
+//   .then((users)=> {
+//     res.render('index', {title: "Users:", users: users.users});
+//   })
+//   .catch(()=> {
+//     res.render('error', {error: err});
+//   })
+// });
+
+app.get('/', async (req,res) => {
+  try {
+    const users = await getUsers();
     res.render('index', {title: "Users:", users: users.users});
-  })
-  .catch(()=> {
+  } catch(err) {
     res.render('error', {error: err});
-  })
+  }
 });
 
 
